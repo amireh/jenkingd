8445f1960121479fd71d31a47c3b47df8b77471f
Ahmad Amireh <ahmad.amireh@gmail.com>
2014-06-24 12:37:23 +0300

    no longer relying on jQuery

--- gerrit-scraper.js
+++ gerrit-scraper.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env phantomjs
 
 var page = require('webpage').create();
-var xsrfKey;
 var servicePage = require('webpage').create();
 var extractLinks = require('./extract_links');
 var sniffXSRFToken = require('./link_scraper/sniff_xsrf_token');
@@ -15,34 +14,36 @@ var output = [];
 var requestCount = PATCHES.length;
 var errorCount = 0;
 
+page.settings.loadImages = false;
 page.customHeaders = {
   'Authorization': 'Basic YWhtYWQ6ZW5zdHJ1Y3R1cmVUdW9uI2xhODk='
 };
 
-// page.settings.userName = 'ahmad';
-// page.settings.password = 'enstructureTuon#la89';
-// page.settings.webSecurityEnabled = false;
-page.settings.loadImages = false;
+// Sniff the XSRF token:
+page.onResourceReceived = function(response) {
+  var xsrfKey = sniffXSRFToken(response);
 
-// page.onResourceRequested = function(requestData, networkRequest) {
-//   console.log('Request to:', requestData.url);
+  if (xsrfKey) {
+    console.log('XSRF token:', xsrfKey);
 
-//   if (requestData.url.match(/capabilities/)) {
-//     console.log('Request (#' + requestData.id + '): ' + JSON.stringify(requestData));
-//     console.log('Request headers:', JSON.stringify(requestData.headers));
-//   }
-// };
+    page.onResourceReceived = null;
 
+    dispatch(xsrfKey);
+  }
+};
+
+// Extract the links from the ChangeDetailService output:
 page.onCallback = function(resp) {
+  console.log('XHR status:', resp.status, resp.success);
+
   if (resp.success) {
     output.push({
       patch: resp.patchId,
       links: extractLinks(resp.body)
     });
-    // phantom.exit(0);
-  } else {
+  }
+  else {
     console.log('XHR failure:', JSON.stringify(resp));
-    // phantom.exit(1);
     errorCount += 1;
   }
 
@@ -52,51 +53,18 @@ page.onCallback = function(resp) {
   }
 };
 
-// Sniff the XSRF token:
-page.onResourceReceived = function(response) {
-  var token = sniffXSRFToken(response);
-
-  if (token) {
-    console.log('XSRF token:', token);
-
-    xsrfKey = token;
-    page.onResourceReceived = null;
-  }
-};
+function dispatch(xsrfKey) {
+  PATCHES.forEach(function(patchId) {
+    console.log('Requesting details for patch:', patchId);
+    page.evaluate(getChangeDetails, patchId, xsrfKey);
+  });
+}
 
 page.open('https://gerrit.instructure.com/', function(status) {
-  var ready;
-  var dispatcher;
-
   console.log('Page opened successfully?', status === 'success');
 
   setTimeout(function() {
     console.log('Timed out.');
     phantom.exit(1);
   }, TIMEOUT);
-
-  setInterval(function() {
-    ready = page.evaluate(function() {
-      try { return !!$; }
-      catch(e) {
-        return false;
-      }
-    });
-  }, 250);
-
-  dispatcher = setInterval(function() {
-    if (!xsrfKey || !ready) {
-      return;
-    }
-
-    console.log('Getting patchset information.');
-
-    PATCHES.forEach(function(patchId) {
-      page.evaluate(getChangeDetails, patchId, xsrfKey);
-    });
-
-    clearInterval(dispatcher);
-    dispatcher = null;
-  }, 1000);
-  // page.close();
 });
--- link_scraper/get_change_details.js
+++ link_scraper/get_change_details.js
@@ -1,41 +1,60 @@
 // var REQUEST_ID = 0;
 
 module.exports = function getChangeDetails(patchId, xsrfKey) {
-  var data = JSON.stringify({
+  var SERVICE_URL = '/gerrit_ui/rpc/ChangeDetailService';
+  var xhr;
+  var data = {
     jsonrpc: '2.0',
     method: 'changeDetail',
+    xsrfKey: xsrfKey,
     params: [{
       id: patchId
-    }],
-    // id: ++REQUEST_ID,
-    xsrfKey: xsrfKey
-  });
+    }]
+  };
 
-  $.ajax({
-    type: 'POST',
-    url: '/gerrit_ui/rpc/ChangeDetailService',
-    headers: {
-      'Accept': 'application/json,application/json,application/jsonrequest',
-      'Content-Type': 'application/json; charset=UTF-8'
-    },
-    data: data,
-    success: function(resp, a, xhr) {
+  xhr = new XMLHttpRequest();
+
+  xhr.open('POST', SERVICE_URL, true);
+  xhr.onreadystatechange = function() {
+    // else
+    if (xhr.readyState === 4) {
       window.callPhantom({
-        success: true,
         patchId: patchId,
         status: xhr.status,
-        body: resp
-      });
-    },
-    error: function(error, a, b) {
-      window.callPhantom({
-        patchId: patchId,
-        failed: true,
-        status: error.status,
-        body: error.responseText
+        success: xhr.status === 200,
+        body: JSON.parse(xhr.responseText)
       });
     }
-  });
+  };
+  xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+  xhr.setRequestHeader('Accept', 'application/json,application/json,application/jsonrequest');
+  xhr.send(JSON.stringify(data));
+
+  // $.ajax({
+  //   type: 'POST',
+  //   url: '/gerrit_ui/rpc/ChangeDetailService',
+  //   headers: {
+  //     'Accept': 'application/json,application/json,application/jsonrequest',
+  //     'Content-Type': 'application/json; charset=UTF-8'
+  //   },
+  //   data: data,
+  //   success: function(resp, a, xhr) {
+  //     window.callPhantom({
+  //       success: true,
+  //       patchId: patchId,
+  //       status: xhr.status,
+  //       body: resp
+  //     });
+  //   },
+  //   error: function(error, a, b) {
+  //     window.callPhantom({
+  //       patchId: patchId,
+  //       failed: true,
+  //       status: error.status,
+  //       body: error.responseText
+  //     });
+  //   }
+  // });
 
   return true;
 };
